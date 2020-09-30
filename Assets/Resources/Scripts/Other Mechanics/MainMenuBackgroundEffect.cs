622f30829e17bd8b746413766257ff8eb0838279
liviusgrosu <liviusgrosu@gmail.com>
2020-09-29 20:19:08 -0400

    Finished main menu screen

---
+++ Assets/Resources/Scripts/Other Mechanics/MainMenuBackgroundEffect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBackgroundEffect : MonoBehaviour
+{
+    public GameObject effectParticle;
+    public float cycleTime;
+    public List<Transform> spawnersWave1, spawnersWave2;
+
+    private float currEffectTime;
+    private bool currSpawners;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currEffectTime = cycleTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currEffectTime >= cycleTime)
+        {
+            currEffectTime = 0f;
+            currSpawners = !currSpawners;
+            if (currSpawners) SpawnEffectInstant(spawnersWave1);
+            else SpawnEffectInstant(spawnersWave2);
+        }
+        currEffectTime += Time.deltaTime;
+    }
+
+    private void SpawnEffectInstant(List<Transform> spawners)
+    {
+        foreach(Transform spawner in spawners)
+        {
+            GameObject instant = Instantiate(effectParticle, spawner.transform.position, effectParticle.transform.rotation);
+            instant.transform.parent = transform;
+            Destroy(instant, 10f);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Utils/AddConstantForce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantForce : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float xAmount, yAmount, zAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(xAmount, yAmount, zAmount);
+    }
+}
