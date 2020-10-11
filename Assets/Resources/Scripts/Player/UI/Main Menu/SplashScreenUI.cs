11e4ccc2278bf5858a4eb312e86c4c8cecdee44f
liviusgrosu <liviusgrosu@gmail.com>
2020-10-11 13:39:19 -0300

    Added a splash screen

---
+++ Assets/Resources/Scripts/Player/UI/Main Menu/SplashScreenUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreenUI : MonoBehaviour
+{
+    public Text presentedByText, companyNameText;
+    public float fadingIncrement = 0.1f;
+
+    public MainMenuMaster menuMaster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeInText(presentedByText));
+        StartCoroutine(FadeInText(companyNameText));
+    }
+
+    private IEnumerator FadeInText(Text currText)
+    {
+        while(currText.color.a < 1f)
+        {
+            currText.color = new Color(currText.color.r, currText.color.g, currText.color.b, currText.color.a + fadingIncrement);
+            yield return null;
+        }
+        StartCoroutine(WaitAndShowScreen(currText));
+    }
+
+    private IEnumerator WaitAndShowScreen(Text currText)
+    {
+        yield return new WaitForSeconds(1.0f);
+        StartCoroutine(FadeOutText(currText));
+    }
+
+    private IEnumerator FadeOutText(Text currText)
+    {
+        while (currText.color.a > 0f)
+        {
+            currText.color = new Color(currText.color.r, currText.color.g, currText.color.b, currText.color.a - fadingIncrement);
+            yield return null;
+        }
+        menuMaster.ChangeToPage(MainMenuMaster.MenuPage.Title);
+    }
+}
